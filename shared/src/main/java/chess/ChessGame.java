09c61ec43d124ea0e68ea64e4ebbcdf1781b06a6
EthanAdamThompson <eat53@byu.edu>
2026-05-12 22:40:21 -0600

    I still need to figure out how to make the pawns move

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * A class that can manage a chess game, making moves on a board
@@ -35,6 +36,20 @@ public class ChessGame {
     public void setTeamTurn(TeamColor team) {
 
         this.teamTurn = team;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, board);
     }
 
     /**
