30298c94c4cf7d024bd6cbe619e71a1324e5eba0
EthanAdamThompson <eat53@byu.edu>
2026-05-20 16:30:36 -0600

    Created GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataaccess.*;
+import model.GameData;
+
+import java.util.List;
+
 public class GameService {
+    private final DataAccess dataAccess;
+    public GameService(DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
+
+    public record ListGameRequest(String authToken){}
+    public record ListGameResult(List<GameData> games) {}
+
+    public ListGameResult listGames(ListGameRequest req) throws DataAccessException{
+        if(dataAccess.getAuth(req.authToken()) == null) {
+            throw new UnauthorizedException("Error: Unauthorized");
+        }
+        return new ListGameResult(dataAccess.listGames());
+    }
 }
