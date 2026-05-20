c62ca0648304c1ffd223ad7c57be2edbbd9d2ca5
EthanAdamThompson <eat53@byu.edu>
2026-05-20 15:19:34 -0600

    Started Server.java but need to set up userHandler and gameHandler first before it will work.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
-import io.javalin.*;
+import dataaccess.MemoryDataAccess;
+import io.javalin.Javalin;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 
 public class Server {
 
@@ -10,14 +14,29 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        // User endpoints
+        javalin.post("/user", userHandler::register);
+        javalin.post("/session", userHandler::login);
+        javalin.delete("/session", userHandler::logout);
+
+        // Game endpoints
+        javalin.get("/game", gameHandler::listGames);
+        javalin.post("/game", gameHandler::createGame);
+        javalin.put("/game", gameHandler::joinGame);
+
+        // Global exception handler
+        javalin.exception(Exception.class, (e, ctx) ->
+                ctx.status(500).json(new ErrorResponse("Error: " + e.getMessage())));
 
     }
 
+    //Given by default
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
     }
 
+    //Given by default
     public void stop() {
         javalin.stop();
     }
