d4be5e7fc69b205251cd58ac30091596e95819b1
EthanAdamThompson <eat53@byu.edu>
2026-05-20 17:57:12 -0600

    Started the User Service. Forgot to push to git after finishing first part

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,49 @@
 package service;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
 public class UserService {
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    // Register the user
+    public record RegisterUserRequest(String username, String password, String email) {}
+    public record RegisterUserResult(String username, String authToken) {}
+    public RegisterUserResult register(RegisterUserRequest request) throws DataAccessException {
+        if (request.username() == null || request.username().isBlank() || request.password() == null
+                || request.password().isBlank() || request.email() == null || request.email().isBlank()) {
+            throw new BadRequestException("Error: bad request");
+        }
+        if (dataAccess.getUser(request.username()) != null) {
+            throw new AlreadyTakenException("Error: already taken");
+        }
+        dataAccess.createUser(new UserData(request.username(), request.password(), request.email()));
+        String token = UUID.randomUUID().toString();
+        dataAccess.createAuth(new AuthData(token, request.username()));
+        return new RegisterUserResult(request.username(), token);
+    }
+
+    // User Login
+    public record LoginUserRequest(String username, String password) {}
+    public record LoginUserResult(String username, String authToken) {}
+    public LoginUserResult login(LoginUserRequest request) throws DataAccessException {
+        if (request.username() == null || request.username().isBlank() || request.password() == null || request.password().isBlank()) {
+            throw new BadRequestException("Error: bad request");
+        }
+        UserData user = dataAccess.getUser(request.username());
+        if (user == null || !user.password().equals(request.password())) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+        String token = UUID.randomUUID().toString();
+        dataAccess.createAuth(new AuthData(token, request.username()));
+        return new LoginUserResult(request.username(), token);
+    }
+
 }
