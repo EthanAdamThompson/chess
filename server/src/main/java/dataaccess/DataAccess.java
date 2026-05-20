d9d58c2d09d5b9dc0865a2d8c31012b80b04d566
EthanAdamThompson <eat53@byu.edu>
2026-05-20 15:44:41 -0600

    Doing DataAccess stuff

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,4 +1,26 @@
 package dataaccess;
 
-public class DataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.List;
+
+public interface DataAccess {
+
+    // Clear
+    void clear() throws DataAccessException;
+
+    // For Authenticating
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authTokne) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    //Creating the User
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+
+
+
 }
