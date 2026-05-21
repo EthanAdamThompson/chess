791c9ccb17f6a4a92de333e19f701e49e4dbc422
EthanAdamThompson <eat53@byu.edu>
2026-05-20 18:10:44 -0600

    Start User Handler

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,4 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
+import service.UserService;
+
 public class UserHandler {
+
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public UserHandler(UserService userService) {
+        this.userService = userService;
+    }
 }
