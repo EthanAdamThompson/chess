625a46d2958a5621012aeb7edb7903fd06047bb7
EthanAdamThompson <eat53@byu.edu>
2026-05-20 19:51:10 -0600

    created ClearServiceTests

--- server/src/test/java/service/ClearServiceTests.java
+++ server/src/test/java/service/ClearServiceTests.java
@@ -1,4 +1,24 @@
 package service;
 
+import dataaccess.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ClearServiceTests {
+    @Test
+    void clearSuccess() throws Exception {
+        var dataAccess = new MemoryDataAccess();
+        var userService = new UserService(dataAccess);
+        var gameService = new GameService(dataAccess);
+        var clearService = new ClearService(dataAccess);
+
+        String token = userService.register(new UserService.RegisterUserRequest("user", "pw", "u@u.com")).authToken();
+        gameService.createGame(new GameService.CreateGameRequest(token, "MyGame"));
+
+        assertDoesNotThrow(clearService::clear);
+
+        // After clearing, same username should register fine again
+        assertDoesNotThrow(() ->
+                userService.register(new UserService.RegisterUserRequest("user", "pw", "u@u.com")));
+    }
 }
