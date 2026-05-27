0422b23e832596c1a3590865f9261b3d2280dd8c
EthanAdamThompson <eat53@byu.edu>
2026-05-27 16:06:48 -0600

    Changed the Create Table

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -12,9 +12,8 @@ public class MySqlAuthDAO {
     private final String[] createStatements = {
             """
         CREATE TABLE IF NOT EXISTS users (
-            username VARCHAR(50) NOT NULL PRIMARY KEY,
-            password VARCHAR(255) NOT NULL,
-            email VARCHAR(100) NOT NULL
+            authToken VARCHAR(255) NOT NULL PRIMARY KEY,
+            username VARCHAR(50) NOT NULL
         )
         """
     };
