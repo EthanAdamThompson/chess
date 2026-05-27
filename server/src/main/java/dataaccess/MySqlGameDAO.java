1fa4b001897e03954957288f61cf09ad3021f64e
EthanAdamThompson <eat53@byu.edu>
2026-05-27 14:59:34 -0600

    Copy and pasted @Override stuff from UserDAO to GameDAO

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -2,6 +2,9 @@ package dataaccess;
 import chess.ChessGame;
 import com.google.gson.Gson;
 import model.GameData;
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +36,39 @@ public class MySqlGameDAO {
                     ps.executeUpdate();
                 }
             }
+        } catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        var sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        String hashed = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, user.username());
+            ps.setString(2, hashed);
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch (SQLException exception) {
+            throw new DataAccessException("User already exists: " + user.username());
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        var sql = "SELECT username, password, email FROM users WHERE username=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"));
+            }
+            return null;
         } catch (SQLException exception) {
             throw new DataAccessException(exception.getMessage());
         }
