ae0a695ff00feab52d7e50264491efbbf28c2959
EthanAdamThompson <eat53@byu.edu>
2026-05-26 22:54:54 -0600

    progress on MySqlUserDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -7,4 +7,26 @@ public class MySqlUserDAO {
     public MySqlUserDAO() throws DataAccessException {
         configureDatabase();
     }
+    private final String[] createStatements = {
+            """
+        CREATE TABLE IF NOT EXISTS users (
+            username VARCHAR(50) NOT NULL PRIMARY KEY,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(100) NOT NULL
+        )
+        """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 }
