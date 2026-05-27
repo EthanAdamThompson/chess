31c628cd114a729307bca154a5bae38465c44bcf
EthanAdamThompson <eat53@byu.edu>
2026-05-26 23:12:25 -0600

    Still trying to figure out how to get BCrypt to work. Google says I may need to use refresh MAVEN but that doesnt seem to work

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -3,7 +3,7 @@ import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 import java.sql.SQLException;
 
-public class MySqlUserDAO {
+public class MySqlUserDAO implements UserDAO{
     public MySqlUserDAO() throws DataAccessException {
         configureDatabase();
     }
@@ -25,8 +25,40 @@ public class MySqlUserDAO {
                     ps.executeUpdate();
                 }
             }
-        } catch (SQLException e) {
-            throw new DataAccessException(e.getMessage());
+        } catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        var sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        String hashed = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, user.username());
+            ps.setString(2, hashed);
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch (SQLException exception) {
+            throw new DataAccessException("User already exists: " + user.username());
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        var sql = "SELECT username, password, email FROM users WHERE username=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"));
+            }
+            return null;
+        } catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
         }
     }
 }
