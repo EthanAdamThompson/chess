9d95b136c160d197cae445416435532ee698d838
EthanAdamThompson <eat53@byu.edu>
2026-05-27 14:56:04 -0600

    Started the work on MySqlGameDAO

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -12,4 +12,29 @@ public class MySqlGameDAO {
     public MySqlGameDAO() throws DataAccessException {
         configureDatabase();
     }
+    // Creat the string to display for the statements
+    private final String[] createStatements = {
+            """
+        CREATE TABLE IF NOT EXISTS games (
+            gameID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            whiteUsername VARCHAR(50),
+            blackUsername VARCHAR(50),
+            gameName VARCHAR(100) NOT NULL,
+            game TEXT NOT NULL
+        )
+        """
+    };
+    // Identical to User
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.executeUpdate();
+                }
+            }
+        } catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
 }
