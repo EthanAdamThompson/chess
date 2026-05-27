3b1e6e9a451f907ca87c527e93b52973f3aca3f7
EthanAdamThompson <eat53@byu.edu>
2026-05-26 21:57:56 -0600

    Deleting Redundant Code in Data Access.java

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -6,23 +6,7 @@ import model.UserData;
 
 import java.util.List;
 
-public interface DataAccess {
-
+public interface DataAccess extends UserDAO, AuthDAO, GameDAO {
     // Clear
     void clear() throws DataAccessException;
-
-    // For Authenticating
-    void createAuth(AuthData auth) throws DataAccessException;
-    AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(String authToken) throws DataAccessException;
-
-    //Creating the User
-    void createUser(UserData user) throws DataAccessException;
-    UserData getUser(String username) throws DataAccessException;
-
-    // Creating Game
-    int createGame(String gameName) throws DataAccessException;
-    GameData getGame(int gameID) throws DataAccessException;
-    List<GameData> listGames() throws  DataAccessException;
-    void updateGame(GameData game) throws DataAccessException;
 }
