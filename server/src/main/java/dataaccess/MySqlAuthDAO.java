da2137c9766282dc77698e5c8071772417a2c85b
EthanAdamThompson <eat53@byu.edu>
2026-05-27 16:05:48 -0600

    Set up MySqlAuth for finishing

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -31,6 +31,8 @@ public class MySqlAuthDAO {
             throw new DataAccessException(exception.getMessage());
         }
     }
+
+    // void createAuth(AuthData auth) throws DataAccessException;
     @Override
     public void createUser(UserData user) throws DataAccessException {
         var sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
@@ -45,7 +47,7 @@ public class MySqlAuthDAO {
             throw new DataAccessException("User already exists: " + user.username());
         }
     }
-
+    // AuthData getAuth(String authToken) throws DataAccessException;
     @Override
     public UserData getUser(String username) throws DataAccessException {
         var sql = "SELECT username, password, email FROM users WHERE username=?";
@@ -63,4 +65,23 @@ public class MySqlAuthDAO {
             throw new DataAccessException(exception.getMessage());
         }
     }
+    // void deleteAuth(String authToken) throws DataAccessException;
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        var sql = "SELECT username, password, email FROM users WHERE username=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"));
+            }
+            return null;
+        } catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
+
 }
