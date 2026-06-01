4e5e761a6ad6bd12e668f20b26ec5dbe17ff7a08
EthanAdamThompson <eat53@byu.edu>
2026-05-31 16:07:05 -1000

    Created first test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -20,10 +20,24 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws Exception {
+        // clear the database between each test so they don't interfere
+        facade.clear();
+    }
+
+    // Testing for Register
+    @Test
+    void registerSuccess() throws Exception {
+        var auth = facade.register("Bobby", "password1234", "bobby@email.com");
+        assertNotNull(auth.authToken());
+        assertTrue(auth.authToken().length() > 10);
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerDuplicateUsername() throws Exception {
+        facade.register("Alexa", "passwordabcd", "alexa@email.com");
+        assertThrows(Exception.class, () -> facade.register("Alexa", "password2", "alexa2@email.com"));
     }
 
 }
