34f5f898e7d05f7429b7a9f7998d2b02a06bce57
EthanAdamThompson <eat53@byu.edu>
2026-06-08 20:12:57 -0600

    implementing on connect

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -28,7 +28,7 @@ public class Server {
         var clearService = new ClearService(dataAccess);
         var userHandler = new UserHandler(userService);
         var gameHandler = new GameHandler(gameService);
-        var webHandler = new WebHandler();
+        var webHandler = new WebHandler(gameService, userService);
 
         javalin = Javalin.create(config -> {
             config.staticFiles.add("web");
--- server/src/main/java/server/WebHandler.java
+++ server/src/main/java/server/WebHandler.java
@@ -1,8 +1,24 @@
 package server;
 
 import io.javalin.websocket.WsContext;
+import io.javalin.websocket.WsMessageContext;
+import service.GameService;
+import service.UserService;
+import com.google.gson.Gson;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebHandler {
+    private final GameService gameService;
+    private final UserService userService;
+    private final Gson gson = new Gson();
+    private final Map<Integer, Set<WsContext>> gameSessions = new ConcurrentHashMap<>();
+
+    public WebHandler(GameService gameService, UserService userService) {
+        this.gameService = gameService;
+        this.userService = userService;
+    }
 
     public void onConnect(WsContext ctx) {
     }
