726c9baadcd171504fc14ad0b7bf4051b0b59bb2
EthanAdamThompson <eat53@byu.edu>
2026-06-09 15:29:59 -0600

    Updated WebFacade

--- client/src/main/java/client/WebFacade.java
+++ client/src/main/java/client/WebFacade.java
@@ -5,7 +5,9 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 import java.net.URI;
 import jakarta.websocket.*;
-import java.net.URI;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.ErrorMessage;
+import websocket.messages.NotificationMessage;
 
 @ClientEndpoint
 public class WebFacade {
@@ -31,8 +33,17 @@ public class WebFacade {
 
     @OnMessage
     public void onMessage(String message) {
-        ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
-        messageHandler.onMessage(serverMessage);
+        var jsonObj = gson.fromJson(message, com.google.gson.JsonObject.class);
+        String typeStr = jsonObj.get("serverMessageType").getAsString();
+        ServerMessage.ServerMessageType type = ServerMessage.ServerMessageType.valueOf(typeStr);
+
+        ServerMessage typed = switch (type) {
+            case LOAD_GAME    -> gson.fromJson(message, LoadGameMessage.class);
+            case ERROR        -> gson.fromJson(message, ErrorMessage.class);
+            case NOTIFICATION -> gson.fromJson(message, NotificationMessage.class);
+        };
+
+        messageHandler.onMessage(typed);
     }
 
     @OnClose
